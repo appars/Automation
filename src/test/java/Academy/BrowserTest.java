c22770ad7943c578ad4f4126f278c67fbd361b22
Appar <32193913+appars@users.noreply.github.com>
2025-09-02 10:19:20 +0530

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -1,31 +1,41 @@
 package Academy;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserTest {
 
-	
-	@Test
-	public void getData()
-	{
-		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
-		driver.get("https://apparacademy-e4bybmd8cudcaqet.canadacentral-01.azurewebsites.net/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
-		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("ApparAcademy.com International Academy"));
-		driver.close();
-	
-		
-		
-	}
+    @Test
+    public void getData() {
+        System.out.println("Hello Guys");
+
+        // Auto-download and configure the correct ChromeDriver
+        WebDriverManager.chromedriver().setup();
+
+        // Options: headless mode is safer on build agents
+        ChromeOptions options = new ChromeOptions();
+        // Uncomment the line below if running in CI/CD:
+        // options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            driver.get("https://apparacademy-e4bybmd8cudcaqet.canadacentral-01.azurewebsites.net/webapp/");
+            String text = driver.findElement(By.cssSelector("h1")).getText();
+            System.out.println("Page header: " + text);
+
+            Assert.assertTrue(
+                "Header text does not match!",
+                text.equalsIgnoreCase("ApparAcademy.com International Academy")
+            );
+        } finally {
+            // Always close browser
+            driver.quit();
+        }
+    }
 }
